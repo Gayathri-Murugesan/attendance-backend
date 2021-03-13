53044e5d9ebc5f964d9881d8a49249e247aceaea
Joby Bejoy <57924079+jobybejoy@users.noreply.github.com>
2021-03-13 13:40:22 -0500

    New Project Structure

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# attendance-backend
-Django backend application. 
+django-admin 
+
+python manage.py startapp app
+
+add app into `installed` app
+
+```
+...
+INSTALLED_APPS = [
+    ...
+    'app',
+]
+...
+
+```
+
+
+To run the application
+```
+python manage.py runserver
+```
--- backend/urls.py
+++ backend/urls.py
@@ -1,4 +1,4 @@
-"""project1 URL Configuration
+"""backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -13,14 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-#from project1 import app1
-#from project1 import app1
-from django.urls.conf import include
-import project1
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('app1.urls')),
+    path('accounts/', include('app.urls')),
 ]
