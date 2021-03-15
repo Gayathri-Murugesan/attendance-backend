8e67cab9705ab35e511f1c026e1f810ad20869fd
Joby Bejoy <57924079+jobybejoy@users.noreply.github.com>
2021-03-14 22:38:25 -0400

    Registered All Models to admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.class_room)
+admin.site.register(models.department)
+admin.site.register(models.user_profile)
+admin.site.register(models.course)
+admin.site.register(models.session)
+admin.site.register(models.course_enrolled)
+admin.site.register(models.attendance)
